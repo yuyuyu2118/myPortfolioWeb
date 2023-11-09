c8e88ace8ae7e1eae802bb46b09de35e60c643bc
yuyuyu2118 <yuyuyu2118@gmail.com>
2023-11-09 10:16:14 +0900

    Counter

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CounterComponents />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CounterComponents from './components/CounterComponents.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CounterComponents
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/CounterComponents.vue
@@ -0,0 +1,129 @@
+<template>
+    <div>
+        <p>Counte: {{ count }}</p>
+        <button @click="increment5">+5</button>
+        <button @click="increment">+</button>
+        <button @click="decrement">-</button>
+        <button @click="decrement5">-5</button>
+
+        <input type="number" v-model="inputValue" placeholder="Enter a number" />
+        <button @click="incrementCustom">+ Custom</button>
+        <button @click="decrementCustom">- Custom</button>
+        <button @click="reset">reset</button>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+//変更を監視するためにrefをインポート
+import { ref } from 'vue'
+
+export default {
+    name: 'CounterComponents',
+    setup() {
+        const count = ref(0)
+        const inputValue = ref(0)
+        const message = ref('')
+        const increment = () => {
+            count.value++
+            if (count.value > 10) {
+                count.value = 10
+                message.value = '10以上はカウントできません'
+            }
+            // messageを戻す
+            if (count.value < 10) {
+                message.value = ''
+            }
+        }
+        const increment5 = () => {
+            count.value += 5
+            if (count.value > 10) {
+                count.value = 10
+                message.value = '10以上はカウントできません'
+            }
+            // messageを戻す
+            if (count.value < 10) {
+                message.value = ''
+            }
+        }
+        const incrementCustom = () => {
+            count.value += Number(inputValue.value)
+            if (count.value > 10) {
+                count.value = 10
+                message.value = '10以上はカウントできません'
+            }
+            // messageを戻す
+            if (count.value < 10) {
+                message.value = ''
+            }
+        }
+        const decrement = () => {
+            count.value--
+            if (count.value < 0) {
+                count.value = 0
+                message.value = '0未満はカウントできません'
+            }
+            // messageを戻す
+            if (count.value > 0) {
+                message.value = ''
+            }
+        }
+        const decrement5 = () => {
+            count.value -= 5
+            if (count.value < 0) {
+                count.value = 0
+                message.value = '0未満はカウントできません'
+            }
+            // messageを戻す
+            if (count.value > 0) {
+                message.value = ''
+            }
+        }
+        const decrementCustom = () => {
+            count.value -= Number(inputValue.value)
+            if (count.value < 0) {
+                count.value = 0
+                message.value = '0未満はカウントできません'
+            }
+            // messageを戻す
+            if (count.value > 0) {
+                message.value = ''
+            }
+        }
+        const reset = () => {
+            count.value = 0
+            message.value = ''
+        }
+        return {
+            count,
+            increment,
+            increment5,
+            incrementCustom,
+            decrement,
+            decrement5,
+            decrementCustom,
+            reset,
+            message,
+            inputValue
+        }
+    }
+}
+</script>
+
+<style scoped>
+p {
+    font-size: 2em;
+    color: #42b983;
+}
+
+button {
+    font-size: 1em;
+    margin-right: 10px;
+    padding: 5px 10px;
+    border-radius: 5px;
+    border: 1px solid #42b983;
+    color: #42b983;
+    background: #fff;
+    cursor: pointer;
+}
+</style>
